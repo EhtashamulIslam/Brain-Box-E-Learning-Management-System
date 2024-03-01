26553fafd374f383d7f1017cb0215b6dd1f3d40f
Ankur <ankurwarrior5@gmail.com>
2024-03-01 20:14:52 +0600

    Sprint 2 code and images

--- view/classRoom.php
+++ view/classRoom.php
@@ -1,11 +1,14 @@
 <?php 
 include("header.php");
+
 $user_array = "";
 $courseName = "";
 $sec = "";
 $body = "";
 $post_id = "";
 $searchedPost = "";
+
+//fetching class room details
 $classCode = $_GET['classCode'];
 $user_details_query = mysqli_query($con, "SELECT * FROM createclass WHERE courseCode='$classCode'");
 $user_array = mysqli_fetch_array($user_details_query);
@@ -15,17 +18,25 @@ $classMates  = $user_array['student_array'];
 $classMates = str_replace(',', ' ', $classMates);
 $array = explode(" ", $classMates);
 $classID = $user_array['id'];
+
+//fetching teacher details
 $teacherName = $user_array['username'];
 $user_details_query2 = mysqli_query($con, "SELECT * FROM users WHERE username='$teacherName'");
 $teacherDetails = mysqli_fetch_array($user_details_query2);
+
+//when hitting the post 
 if (isset($_POST['post'])) {
     $post = new Post($con, $userLoggedIn2, $classCode);
     $post->submitPost($_POST['post_text'], 'none', 'none', $teacherName);
 }
+
+//edit
 if (isset($_GET['post_id'])) {
     $post_id = $_GET['post_id'];
     $data_query = mysqli_query($con, "SELECT body FROM posts WHERE id='$post_id'");
     $body = mysqli_fetch_array($data_query);
+    
+
     echo '
 	<script>
 		$(document).ready(function(){
@@ -33,34 +44,46 @@ if (isset($_GET['post_id'])) {
 		});
 	</script>
 	';
-} 
+} //edit
 if (isset($_POST['update'])) {
     $post = new Post($con, $userLoggedIn2, $classCode);
     $post->submitEditPost($_POST['editedPost_text'], $post_id);
     header("Location: classRoom.php?classCode=$classCode");
 }
+//edit
 if (isset($_POST['cancel'])) {
     header("Location: classRoom.php?classCode=$classCode");
 }
+
+//when uploading files
+
 if (isset($_POST['upload'])) {
+
     $file = $_FILES['file'];
+
     $fileName = $_FILES['file']['name'];
     $fileSize = $_FILES['file']['size'];
     $fileType = $_FILES['file']['type'];
     $fileTmpName = $_FILES['file']['tmp_name'];
     $fileError = $_FILES['file']['error'];
+
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
+
     $allowed  = array('jpg', 'jpeg', 'png', 'pdf', 'docx', 'doc', 'xlsx', 'pptx', 'ppt');
     $res = str_replace($allowed, "", $fileName);
+
     if (in_array($fileActualExt, $allowed)) {
         if ($fileError === 0) {
             if ($fileSize < 1000000000000) {
                 $fileNameNew = uniqid(" ", true) . "." . $fileActualExt;
                 $fileDestination = 'uploads/' . $res . $fileNameNew;
-                move_uploaded_file($fileTmpName, $fileDestination); 
+                move_uploaded_file($fileTmpName, $fileDestination); //file uploaded okay
+
                 $post = new Post($con, $userLoggedIn, $classCode);
                 $post->submitPost($_POST['assignment_text'], $fileNameNew, $fileDestination,$teacherName);
+                //$post->getFileDestination($fileDestination); 
+
                 header("Location: classRoom.php?classCode=$classCode&uploadsuccess");
             } else {
                 echo "your file is too big";
@@ -72,7 +95,8 @@ if (isset($_POST['upload'])) {
         echo "You can't upload file of this";
     }
 }
-if (isset($_GET['uploadsuccess'])) {
+
+if (isset($_GET['uploadsuccess'])) {   // hold back the assignment div(#second) after delete or upload
     echo '<script>
                      $(document).ready(function(){
                          $("#first").hide();
@@ -81,12 +105,16 @@ if (isset($_GET['uploadsuccess'])) {
                        </script>
                        ';
 }
+
 if(isset($_POST['search__btn'])){
     $searchedPost = $_POST['searched_text'];
     header("Location: search.php?classCode=$classCode&searchedPost=$searchedPost");
 }
 ?>
+
 <div class=Wrapper2>
+
+
     <div class="user_details cloumn">
         <h1> <?php echo $courseName ?></h1>
         <p style='line-height:30px; display: inline-block;'>Section: <?php echo $sec ?>
@@ -97,14 +125,26 @@ if(isset($_POST['search__btn'])){
                 <input type="text" placeholder='Search posts' autocomplete='off'  id='search-bar' name='searched_text'><button id="search__btn" name="search__btn"><i class="fas fa-search"></i></button>
         </form> 
         <div class="assignment_box">
+            <a href="timeline.php" id="timelineBtn"><i class="far fa-file-word" style='margin-right: 5px;'></i>Timeline</a>
+
+            <a href="email.php" id="emailBtn"><i class="fab fa-megaport" style='margin-right: 5px;'></i>Email</a>
+            <a href="#" id="postBtn"><i class="fab fa-megaport" style='margin-right: 5px;'></i>Post</a>
+            
+            <a href="#" id="assignmentBtn"><i class="far fa-file-word" style='margin-right: 5px;'></i>Assignment</a>
+
+            <?php  if(isset($_POST['upload'])) {
+             echo'<span class="assgn-Btn_notification_badge" id="unread_notification"></span>';   
+            }?>
         </div>
     </div>
+
     <div id="modal">
         <div id="modal_container">
             <span id="close_btn">&times;</span>
             <p id="code_modal"><?php echo $classCode ?></p>
         </div>
     </div>
+
     <div id="modal2">
         <div id="modal_bg"></div>
         <div id="edit_box">
@@ -115,6 +155,7 @@ if(isset($_POST['search__btn'])){
             </form>
         </div>
     </div>
+
     <div class="people_column">
        <h4>Instructor:</h4><a href="<?php echo $teacherName; ?>"><img src='<?php echo $teacherDetails['profilePic'] ?>' width='50'><?php echo $teacherDetails['first_name'] . " " . $teacherDetails['last_name'] ?></a>
         <br>
@@ -124,6 +165,21 @@ if(isset($_POST['search__btn'])){
         echo "<p>Classmates: </p>"; ?>
              <?php $stundentsName->getStudentsUserName($array) ?>
     </div>
+
+    <div class="main_column">
+        <div id="first">
+            <form class="post_form" method="POST">
+                <textarea name='post_text' id='post_text_area' placeholder='Share something with your class'></textarea>
+                <input type='submit' name='post' id='post_button' value='post'>
+                <hr>
+            </form>
+
+            <?php
+            $post = new Post($con, $userLoggedIn, $classCode);
+            $post->loadPosts();
+            ?>
+        </div>
+
         <div id="second">
             <form class="assignment_form" method="POST" enctype="multipart/form-data">
                 <input type="file" name="file" id="fileToUpload">
@@ -136,51 +192,85 @@ if(isset($_POST['search__btn'])){
             $post->loadFiles();
             ?>
         </div>
+          
+         
     </div>
+
+
 </div>
+
+
 <script>
     var expandBtn = document.getElementById('code_expand');
     var modal = document.getElementById("modal");
     var closeBtn = document.getElementById("close_btn");
+
     expandBtn.addEventListener('click', openModal);
+
     closeBtn.addEventListener('click', closeModal);
+
     window.addEventListener('click', clickOutsideModal);
+
     function openModal() {
         modal.style.display = 'block';
     }
+
     function closeModal() {
         modal.style.display = 'none';
     }
+
     function clickOutsideModal(e) {
         if (e.target == modal) {
             modal.style.display = 'none';
         }
     }
+
     let editBtn = document.getElementsByClassName('edit_post_btn');
     let modal2 = document.getElementById("modal2");
     let updateBtn = document.getElementById("update_btn");
     let cancelBtn = document.getElementById('update_cancel_btn');
+
+    // for (var i = 0; i < editBtn.length; i++) {
+    //     editBtn[i].addEventListener('click', openModal2);
+    // }
+
     updateBtn.addEventListener('click', closeModal2);
+    // cancelBtn.addEventListener('click', closeModal2);
+
+    // function openModal2() {
+    //     modal2.style.display = 'block';
+    // }
+
     function closeModal2() {
         modal.style.display = 'none';
     }
+
     $(document).ready(function() {
         $('edit_post_btn').click(function() {
             modal2.style.display = 'block';
         });
     });
+
+
+    //slide up down of post and assignment 
+
+    //on click signup, hide login and show registration form
     $(document).ready(function() {
-        $("#assignmentBtn").click(function() { 
+
+        $("#assignmentBtn").click(function() { //show assignment form and hide post form 
             $("#first").slideUp("slow", function() {
                 $("#second").slideDown("slow");
             });
         });
+
         $("#postBtn").click(function() {
-            $("#second").slideUp("slow", function() { 
+            $("#second").slideUp("slow", function() { //vice versa
                 $("#first").slideDown("slow");
             });
         });
     });
+    
+    
 </script>
 </body>
 
